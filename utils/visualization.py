08d5fb79d6eeab02ca8e19536110e551e3f46c9d
Allliance <alians310322@gmail.com>
2024-04-03 14:48:06 +0330

    Add baseline

---
+++ utils/visualization.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import torchvision
+
+def visualize_samples(dataloader, n, title="Sample"):
+    normal_samples = []
+    abnormal_samples = []
+
+    def to_3_channels(image):
+        if image.shape[0] == 1:
+            return image.repeat(3, 1, 1)
+        return image
+
+    # Collect n x n samples
+    for images, labels in dataloader:
+        for i, l in enumerate(labels):
+            image = to_3_channels(images[i])
+            if len(normal_samples) < n * n and l == 0:
+                normal_samples.append(image)
+            elif len(abnormal_samples) < n * n and l != 0:
+                abnormal_samples.append(image)
+            if len(normal_samples) == n * n and len(abnormal_samples) == n * n:
+                break
+        if len(normal_samples) == n * n and len(abnormal_samples) == n * n:
+            break
+
+    
+    normal_grid = torchvision.utils.make_grid(normal_samples, nrow=n)
+    abnormal_grid = torchvision.utils.make_grid(abnormal_samples, nrow=n)
+
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(18, 8))
+    fig.patch.set_alpha(0)
+    fig.suptitle(title, fontsize=16)
+
+    axs[0].imshow(normal_grid.permute(1, 2, 0))
+    axs[0].set_title('Normal', fontsize=14)
+    axs[0].axis('off')
+
+    axs[1].imshow(abnormal_grid.permute(1, 2, 0))
+    axs[1].set_title('Abnormal', fontsize=14)
+    axs[1].axis('off')
+
+    plt.show()
