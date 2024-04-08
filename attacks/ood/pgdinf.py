617b3a201ff2290a8b0f22aea21777035027ae5b
Allliance <alians310322@gmail.com>
2024-04-08 21:00:10 +0330

    Change structures

--- attacks/ood/pgdinf.py
+++ attacks/ood/pgdinf.py
@@ -1,4 +1,4 @@
-class PGDINF(Attack):
+class PGDLINF(Attack):
     r"""
     PGD in the paper 'Towards Deep Learning Models Resistant to Adversarial Attacks'
     [https://arxiv.org/abs/1706.06083]
@@ -18,22 +18,24 @@ class PGDINF(Attack):
         >>> adv_images = attack(images, labels)
     """
 
-    def __init__(self, model, cost_fn, eps=8/255, alpha=2/255, steps=10, random_start=True):
-        super().__init__("PGDINF", model, cost_fn)
+    def __init__(self, model, target_class=None, eps=8/255, alpha=2/255, steps=10, random_start=True):
+        super().__init__("PGD", model)
         self.eps = eps
         self.alpha = alpha
         self.steps = steps
         self.random_start = random_start
         self.supported_mode = ['default', 'targeted']
+        self.target_class = target_class
 
-    def forward(self, images, labels, **kwargs):
+    def forward(self, images, labels):
         r"""
         Overridden.
         """
 
         images = images.clone().detach().to(self.device)
-        labels = labels.clone().detach().to(self.device).type(torch.FloatTensor).to(device)
+        float_labels = labels.clone().detach().to(self.device).type(torch.FloatTensor).to(device)
         
+        loss = nn.CrossEntropyLoss(reduction='none')
         adv_images = images.clone().detach()
         
         if self.random_start:
@@ -41,11 +43,23 @@ class PGDINF(Attack):
             adv_images = adv_images + \
                 torch.empty_like(adv_images).uniform_(-self.eps, self.eps)
             adv_images = torch.clamp(adv_images, min=0, max=1).detach()
+
+        hend_multipliers = float_labels
+        out_multipliers = 1-float_labels
         
         for _ in range(self.steps):
             adv_images.requires_grad = True
+            outputs = self.get_logits(adv_images)
+            probs = torch.softmax(outputs, dim=1)
             
-            cost = self.cost_fn(model, adv_images, **kwargs)
+            if self.target_class is not None:
+                out_loss = -loss(outputs, torch.ones_like(labels) * self.target_class)
+            else:
+                out_loss = torch.max(probs, dim=1)
+            
+            hend_loss = 1 * (probs.mean(1) - torch.logsumexp(probs, dim=1))
+            
+            cost = torch.dot(hend_multipliers, hend_loss) + torch.dot(out_multipliers, out_loss)
             
             # Update adversarial images
             grad = torch.autograd.grad(cost, adv_images,
