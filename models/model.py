f43512688cd7560928bdd472dfe8761b1b7a4544
Allliance <alians310322@gmail.com>
2024-04-09 22:49:19 +0330

    Fix device error in model

--- models/model.py
+++ models/model.py
@@ -3,13 +3,16 @@ import torch
 _mean = (0.5, 0.5, 0.5)
 _std = (0.5, 0.5, 0.5)
 
-mu = torch.tensor(_mean).view(3,1,1).cuda()
-std = torch.tensor(_std).view(3,1,1).cuda()
+mu = torch.tensor(_mean).view(3,1,1)
+std = torch.tensor(_std).view(3,1,1)
 
 class Model(nn.Module):
-    def __init__(self, backbone, normalize=True):
+    def __init__(self, backbone, device=None, normalize=True):
         super(Model, self).__init__()
         self.backbone = backbone
+        if device:
+            mu = mu.to(device)
+            std = std.to(device)
         self.norm = lambda x: ( x - mu ) / std
 
     def forward(self, x):
