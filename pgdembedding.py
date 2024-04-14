973b2f9879debd9d434d023769b50a85af860cba
Bahar Dibaei Nia <49945055+bhrdbn@users.noreply.github.com>
2024-04-15 00:58:30 +0330

    Create pgdembedding.py

---
+++ pgdembedding.py
@@ -0,0 +1,92 @@
+class PGD_embeddings(Attack):
+    r"""
+    PGD in the paper 'Towards Deep Learning Models Resistant to Adversarial Attacks'
+    [https://arxiv.org/abs/1706.06083]
+    Distance Measure : Linf
+    Arguments:
+        model (nn.Module): model to attack.
+        eps (float): maximum perturbation. (Default: 8/255)
+        alpha (float): step size. (Default: 2/255)
+        steps (int): number of steps. (Default: 10)
+        random_start (bool): using random initialization of delta. (Default: True)
+    Shape:
+        - images: :math:`(N, C, H, W)` where `N = number of batches`, `C = number of channels`,        `H = height` and `W = width`. It must have a range [0, 1].
+        - labels: :math:`(N)` where each value :math:`y_i` is :math:`0 \leq y_i \leq` `number of labels`.
+        - output: :math:`(N, C, H, W)`.
+    Examples::
+        >>> attack = torchattacks.PGD(model, eps=8/255, alpha=1/255, steps=10, random_start=True)
+        >>> adv_images = attack(images, labels)
+    """
+
+    def __init__(self, model, in_mean, eps=8/255, alpha=2/255, steps=10, random_start=True):
+        super().__init__("PGD", model)
+        self.eps = eps
+        self.alpha = alpha
+        self.steps = steps
+        self.random_start = random_start
+        self.supported_mode = ['default', 'targeted']
+        self.mean_in = in_mean
+
+    def forward(self, images, labels):
+        r"""
+        Overridden.
+        """
+
+        images = images.clone().detach().to(self.device)
+        new_labels = torch.where(labels == 10, torch.tensor(0), torch.tensor(1))
+
+        float_labels = new_labels.clone().detach().type(torch.FloatTensor).to(self.device)
+
+        
+        
+        adv_images = images.clone().detach()
+        
+        if self.random_start:
+            # Starting at a uniformly random point
+            adv_images = adv_images + \
+                torch.empty_like(adv_images).uniform_(-self.eps, self.eps)
+            adv_images = torch.clamp(adv_images, min=0, max=1).detach()
+
+        in_multipliers = torch.zeros_like(float_labels)
+        out_multipliers = 1-float_labels
+        
+        for _ in range(self.steps):
+            adv_images.requires_grad = True
+            outputs = self.get_logits(adv_images)
+            features = self.model.get_embeds_and_logit_from_forward(adv_images)
+            #loss_in = torch.norm(features - self.mean_out)
+            loss_out = - torch.norm(features - self.mean_in, dim=1)
+            
+            
+            
+            
+#             out_loss= -loss(outputs, int_tar)
+            
+#             Calculate loss
+            
+#             First calculate the loss of MSP
+#             For these samples (outliers), we wish to increase msp
+            
+#             probs = torch.softmax(outputs, dim=1)
+#             msp, _ = torch.max(probs, dim=1)
+#             msp_loss = msp
+            
+            
+            # We then use Hendrycks loss here
+            # For these samples (inliers), we wish to increase msp
+            
+            
+            #hend_loss = 1 * (probs.mean(1) - torch.logsumexp(probs, dim=1))
+            
+            cost =  torch.dot(out_multipliers, loss_out)
+            
+            # Update adversarial images
+            grad = torch.autograd.grad(cost, adv_images,
+                                       retain_graph=False, create_graph=False)[0]
+
+            adv_images = adv_images.detach() + self.alpha*grad.sign()
+            delta = torch.clamp(adv_images - images,
+                                min=-self.eps, max=self.eps)
+            adv_images = torch.clamp(images + delta, min=0, max=1).detach()
+
+        return  adv_images
