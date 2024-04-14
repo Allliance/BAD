4311be035e391bea74eb6dd4498b7dd0bf03a5b1
Allliance <alians310322@gmail.com>
2024-04-14 18:47:59 +0330

    Add model loader

---
+++ models/loaders.py
@@ -0,0 +1,24 @@
+import copy
+import pickle 
+from torchvision import transforms
+
+def load_preact(record_path):
+    
+    load_file = torch.load(record_path)
+
+    new_dict = copy.deepcopy(load_file['model'])
+    for k, v in load_file['model'].items():
+        if k.startswith('module.'):
+            del new_dict[k]
+            new_dict[k[7:]] = v
+
+    load_file['model'] = new_dict
+    
+    net = ModelArch(num_classes=num_classes)
+    net.load_state_dict(load_file['model'])
+    
+    model = Model(net, device)
+    model.to(device)
+    model.eval()
+    
+    return model
