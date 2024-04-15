bd53cf889d36c82558d538fd4d664bdc8ba07639
Allliance <alians310322@gmail.com>
2024-04-15 16:02:25 +0330

    Add model dataset

--- detector/datasets.py
+++ detector/datasets.py
@@ -1,22 +1,34 @@
 import os
 
+CLEAN_LABEL = 'clean'
+
 class ModelDataset(Dataset):
-    def __init__(self, root_folder):
-        self.root_folder = root_folder
+    def __init__(self, cleans_folder, bads_folder, model_loader, sample=False, sample_k=5):
         self.classes = ['cleans', 'bads']
         self.model_paths = []
         self.labels = []
-        for cls in self.classes:
-            class_folder = os.path.join(root_folder, cls)
-            label = 0 if cls == 'cleans' else 1
-            for filename in os.listdir(class_folder):
-                if filename.endswith('.pt'):
-                    self.model_paths.append(os.path.join(class_folder, filename))
-                    self.labels.append(label)
-        self.data = [feature_extractor(path) for path in model_paths]
+        self.loader = model_loader
+        for attack_folder in attack_folders:
+            ck_folder = os.path.join(bads_folder, attack_folder)
+            ck_files = [os.path.join(ck_folder, ck_file) for ck_file in os.listdir(ck_folder)]
+            
+            if sample:
+                ck_files = random.sample(ck_files, k=sample_k)
+                
+            self.model_paths += ck_files
+            self.labels += [attack_folder] * len(ck_files)
+            
+        
+        clean_checkpoints = [os.path.join(CLEAN_ROOT, x) for x in os.listdir(CLEAN_ROOT)]
+        if sample:
+            clean_checkpoints = random.sample(clean_checkpoints, sample_k*4)
+            
+        self.model_paths += clean_checkpoints
+        self.labels += [CLEAN_LABEL] * len(clean_checkpoints)
 
     def __len__(self):
-        return len(self.model_paths)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
+        return self.loader(self.model_paths[idx]), self.labels[idx] == CLEAN_LABEL
+    
