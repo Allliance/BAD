fb81c9f4e7ede62b3d6c38fe16fc558083fd7336
Allliance <alians310322@gmail.com>
2024-04-15 18:55:42 +0330

    Apply changes to model dataset

--- detector/datasets.py
+++ detector/datasets.py
@@ -1,24 +1,25 @@
-import os
+import os, random, pathlib
 from torch.utils.data import Dataset
 CLEAN_LABEL = 'clean'
 
+def extract_attack_name(model_path):
+    return 'bad'
+
 class ModelDataset(Dataset):
-    def __init__(self, cleans_folder, bad_folders, model_loader, sample=False, sample_k=5):
+    def __init__(self, cleans_folder, bads_folder, model_loader, sample=False, sample_k=5):
         self.model_paths = []
         self.labels = []
         self.loader = model_loader
-        for attack_folder in bad_folders:
-            ck_folder = os.path.join(bads_folder, attack_folder)
-            ck_files = [os.path.join(ck_folder, ck_file) for ck_file in os.listdir(ck_folder)]
-            
-            if sample:
-                ck_files = random.sample(ck_files, k=sample_k)
-                
-            self.model_paths += ck_files
-            self.labels += [attack_folder] * len(ck_files)
+        
+        bad_checkpoints = [str(x) for x in pathlib.Path(bads_folder).iterdir() if str(x).endswith('.pt')]
+        if sample:
+            clean_checkpoints = random.sample(clean_checkpoints, sample_k*4)
+        
+        self.model_paths += bad_checkpoints
+        self.labels += [extract_attack_name(x) for x in bad_checkpoints]
             
         
-        clean_checkpoints = [os.path.join(CLEAN_ROOT, x) for x in os.listdir(CLEAN_ROOT)]
+        clean_checkpoints = [str(x) for x in pathlib.Path(cleans_folder).iterdir() if str(x).endswith('.pt')]
         if sample:
             clean_checkpoints = random.sample(clean_checkpoints, sample_k*4)
             
@@ -30,4 +31,3 @@ class ModelDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.loader(self.model_paths[idx]), self.labels[idx] == CLEAN_LABEL
-    
