7948feb78d61ca8634d6027a6ee728abd4f81f93
Allliance <alians310322@gmail.com>
2024-04-16 00:35:35 +0330

    Add changes to get max diff

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ import umap
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-def get_features(model, loader, DEVICE, attack=None, progress=False):
+def get_features(model, loader, attack=None, progress=False):
     features = []
     outputs = []
 
@@ -48,40 +48,54 @@ def get_features(model, loader, DEVICE, attack=None, progress=False):
     return gaussian_features, cifar_features
 
 # score in [l2, cosine]
-def get_max_diff(model,testloader, attack_config=None, score='l2', use_in=True, progress=False):
+def get_max_diff(model, testloader, attack_config=None, score='l2', use_in=True, progress=False):
     max_l2 = 0
-    tq = range(10)
-    if progress:
-        tq = tqdm(range(10))
     
     attack_class = attack_config['attack_class']
     attack_config.pop('attack_class')
-    best_target = None
     
-    v_out_b, v_in_b= get_features(model, testloader, device, attack=None)
-    for i in tq:
-        attack_config['target_class'] = i
+    v_out_b, v_in_b = get_features(model, testloader, attack=None)
+    v_in_b_mean = np.mean(v_in_b, axis=0)
+    v_out_b_mean = np.mean(v_out_b, axis=0)
+    if attack_config.get('target_class') is not None:
+        best_target = None
+        tq = range(10)
+        if progress:
+            tq = tqdm(range(10))
+        for i in tq:
+            attack_config['target_class'] = i
+            attack = attack_class(model, **attack_config)
+            v_out_a, v_in_a = get_features(model, testloader,  attack=attack)
+            v_out_a_mean = np.mean(v_out_a, axis=0)
+            v_in_a_mean = np.mean(v_in_a, axis=0)
+            if use_in:
+                diff_a = (v_out_a_mean - v_in_a_mean)
+                diff_b = (v_out_b_mean - v_in_b_mean)
+                #cosine = np.dot(diff_a, diff_b)/(norm(diff_a)*norm(diff_b))
+                l2 = norm(diff_a - diff_b)     
+                if l2 > max_l2:
+                    max_l2 = l2
+            else:
+                diff = v_out_a_mean - v_out_b_mean
+                l2 = norm(diff)
+                if l2 > max_l2:
+                    max_l2 = l2
+                    best_target = i
+        return best_target, max_l2
+    else:
         attack = attack_class(model, **attack_config)
-        v_out_a, v_in_a = get_features(model, testloader, device, attack=attack)
-        v_out_b_mean = np.mean(v_out_b, axis=0)
+        v_out_a, v_in_a = get_features(model, testloader,  attack=attack)
         v_out_a_mean = np.mean(v_out_a, axis=0)
-        v_in_b_mean = np.mean(v_in_b, axis=0)
         v_in_a_mean = np.mean(v_in_a, axis=0)
         if use_in:
             diff_a = (v_out_a_mean - v_in_a_mean)
             diff_b = (v_out_b_mean - v_in_b_mean)
-            #cosine = np.dot(diff_a, diff_b)/(norm(diff_a)*norm(diff_b))
-            l2 = norm(diff_a - diff_b)     
-            if l2 > max_l2:
-                max_l2 = l2
+            #score = np.dot(diff_a, diff_b)/(norm(diff_a)*norm(diff_b))
+            score = norm(diff_a - diff_b)
         else:
             diff = v_out_a_mean - v_out_b_mean
-            l2 = norm(diff)
-            if l2 > max_l2:
-                max_l2 = l2
-                best_target = i
-                           
-    return best_target, max_l2
+            score = norm(diff)
+        return score
 
 def ood_sanity_check(testloader, adv_check=True, clean_check=True, target=None):
     print("Sanity check started")
