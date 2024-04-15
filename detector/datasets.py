137106d2609ea75f9cb345fb8827c4fcacc2507a
Allliance <alians310322@gmail.com>
2024-04-15 19:17:02 +0330

    Fix bugs in model dataset

--- detector/datasets.py
+++ detector/datasets.py
@@ -2,8 +2,12 @@ import os, random, pathlib
 from torch.utils.data import Dataset
 CLEAN_LABEL = 'clean'
 
-def extract_attack_name(model_path):
-    return 'bad'
+def extract_models_paths(root_dir):
+    paths = []
+    for root, _, files in os.walk(root_dir):
+        new_paths = [os.path.join(root, file) for file in files if file.endswith('.pt')]
+        paths += new_paths
+    return paths
 
 class ModelDataset(Dataset):
     def __init__(self, cleans_folder, bads_folder, model_loader, sample=False, sample_k=5):
@@ -11,18 +15,17 @@ class ModelDataset(Dataset):
         self.labels = []
         self.loader = model_loader
         
-        bad_checkpoints = [str(x) for x in pathlib.Path(bads_folder).iterdir() if str(x).endswith('.pt')]
+        attack_folders = [os.path.join(bads_folder, x) for x in os.listdir(bads_folder) if os.path.isdir(os.path.join(bads_folder, x))]
+        for attack_folder in attack_folders:
+            bad_checkpoints = extract_models_paths(bads_folder)
+            if sample:
+                bad_checkpoints = random.sample(bad_checkpoints, sample_k)
+            self.model_paths += bad_checkpoints
+            self.labels += [attack_folder] * len(bad_checkpoints)
+        
+        clean_checkpoints = extract_models_paths(cleans_folder)
         if sample:
             clean_checkpoints = random.sample(clean_checkpoints, sample_k*4)
-        
-        self.model_paths += bad_checkpoints
-        self.labels += [extract_attack_name(x) for x in bad_checkpoints]
-            
-        
-        clean_checkpoints = [str(x) for x in pathlib.Path(cleans_folder).iterdir() if str(x).endswith('.pt')]
-        if sample:
-            clean_checkpoints = random.sample(clean_checkpoints, sample_k*4)
-            
         self.model_paths += clean_checkpoints
         self.labels += [CLEAN_LABEL] * len(clean_checkpoints)
 
@@ -30,4 +33,5 @@ class ModelDataset(Dataset):
         return len(self.labels)
 
     def __getitem__(self, idx):
+        
         return self.loader(self.model_paths[idx]), self.labels[idx] == CLEAN_LABEL
