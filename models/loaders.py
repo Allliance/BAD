6b31f967285eeb3d06f42795f5c157fe3f6f6021
Allliance <alians310322@gmail.com>
2024-04-15 15:38:02 +0330

    Add resnet18 architecture

--- models/loaders.py
+++ models/loaders.py
@@ -4,8 +4,11 @@ from torchvision import transforms
 import torch
 from .base_model import BaseModel as Model
 from .preact import PreActResNet18
+from torchvision.models import resnet18
 
-def load_preact(record_path, device, num_classes=10):
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+def load_preact(record_path, num_classes=10):
     
     load_file = torch.load(record_path)
 
@@ -20,7 +23,21 @@ def load_preact(record_path, device, num_classes=10):
     net = PreActResNet18(num_classes=num_classes)
     net.load_state_dict(load_file['model'])
     
-    model = Model(net, device)
+    model = Model(net)
+    model.to(device)
+    model.eval()
+    
+    return model
+
+def load_resnet(record_path, num_classes=10):
+    state_dict = torch.load(record_path)
+    
+    net = resnet18(num_classes=num_classes)
+    net.load_state_dict(state_dict)
+    
+    feature_extractor = torch.nn.Sequential(*list(net.children())[:-1])
+    
+    model = Model(net, feature_extractor=feature_extractor)
     model.to(device)
     model.eval()
     
