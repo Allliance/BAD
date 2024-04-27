7e368ff170a362b3486fb59d99ac3502b56c07ba
Ali Ansari <47219029+Allliance@users.noreply.github.com>
2024-04-27 16:01:23 +0330

    Remove duplicate code in get_id_dataset and get_ood_dataset

--- data/datasets.py
+++ data/datasets.py
@@ -30,7 +30,7 @@ class SingleLabelDataset(Dataset):
     # defining values in the constructor
     def __init__(self, label, dataset, transform = None, lim=None):
         self.data = [dataset[i][0] for i in range(len(dataset) if lim is None else min(lim, len(dataset)))]
-
+        
         self.label = label
 
     # Getting the data samples
@@ -44,10 +44,13 @@ class SingleLabelDataset(Dataset):
 
 
 class GaussianDataset(Dataset):
-    def __init__(self, label, num_samples=3000, size=32):
+    def __init__(self, label, num_samples=3000, size=32, transform=None):
         self.data = torch.randn((num_samples,3,size,size))
         self.label = label
-        self.transform = transforms.ToTensor()
+        if transform:
+            self.transform = transform
+        else:
+            self.transform = transforms.ToTensor()
         self.to_pil = transforms.ToPILImage()
 
     def __len__(self):
@@ -58,10 +61,13 @@ class GaussianDataset(Dataset):
         return self.transform(sample), self.label
 
 class BlankDataset(Dataset):
-    def __init__(self, label, num_samples=3000, size=32, color=0):
+    def __init__(self, label, num_samples=3000, size=32, color=0,  transform=None):
         self.data = torch.ones((num_samples,3,size,size)) * color
         self.label = label
-        self.transform = transforms.ToTensor()
+        if transform:
+            self.transform = transform
+        else:
+            self.transform = transforms.ToTensor()
         self.to_pil = transforms.ToPILImage()
 
     def __len__(self):
--- data/loaders.py
+++ data/loaders.py
@@ -37,55 +37,37 @@ def filter_labels(dataset, labels):
     return Subset(dataset, indices)
 
 def get_transform(dataset):
-  if dataset in ['rot']:
-    return []
-  if dataset in ['cifar10', 'cifar100', 'gtsrb']:
+  if dataset in ['cifar10', 'cifar100', 'gtsrb', 'SVHN']:
       return normal_transform
-  elif dataset == 'mnist':
+  elif dataset in ['fmnist', 'mnist']:
       return bw_transform
+  elif dataset in ['gaussian', 'blank']:
+      return None
   else:
       raise NotImplementedError
 
-def get_ood_dataset(dataset, out_transform, in_dataset=None, out_label=None, sample_num=1000):
-    if out_transform is None:
-        out_transform = get_transform(dataset)
-        
+def get_dataset(name, transform=None, train=False, custom_label=None, sample_num=3000):
+    if transform is None:
+        transform = get_transform(dataset)
+
     if dataset == 'SVHN':
-        return torchvision.datasets.SVHN(root=ROOT, split='test', download=True, transform=out_transform)
+        return torchvision.datasets.SVHN(root=ROOT, split='train' if train else 'test', download=True, transform=transform)
     elif dataset == 'mnist':
-        return torchvision.datasets.MNIST(root=ROOT, train=False, download=True, transform=out_transform)
+        return torchvision.datasets.MNIST(root=ROOT, train=train, download=True, transform=transform)
+    elif dataset == 'fmnist':
+        return torchvision.datasets.FashionMNIST(root=ROOT, train=train, download=True, transform=transform)
+    elif dataset == 'cifar10':
+        in_dataset = torchvision.datasets.CIFAR10(root=ROOT, train=train,transform=transform, download=True)
     elif dataset =='cifar100':
-        return torchvision.datasets.CIFAR100(root=ROOT, train=False, download=True, transform=out_transform)
+        return torchvision.datasets.CIFAR100(root=ROOT, train=train, download=True, transform=transform)
+    elif dataset == 'gtsrb':
+        in_dataset = GTSRB(train=train,transform=transform, download=True)
     elif dataset == 'gaussian':
         return GaussianDataset(out_label, num_samples=sample_num)
     elif dataset == 'blank':
         return BlankDataset(out_label, color=0, num_samples=sample_num)
-    elif dataset == 'fmnist':
-        return torchvision.datasets.FashionMNIST(root=ROOT, train=False, download=True, transform=out_transform)
-    elif dataset == 'rot' and in_dataset is not None:
-        out_dataset = deepcopy(in_dataset)
-        out_dataset.transform = transforms.Compose([lambda x: rotate(x, 90), out_dataset.transform])
-        return out_dataset
     else:
         raise NotImplementedError
-
-
-def get_id_dataset(dataset, use_train, transform):
-    if transform is None:
-        transform = get_transform(dataset)
-    if dataset == 'cifar10':
-        in_dataset = torchvision.datasets.CIFAR10(root=ROOT, train=use_train,transform=transform, download=True)
-    elif dataset == 'cifar100':
-        in_dataset = torchvision.datasets.CIFAR100(root=ROOT, train=use_train,transform=transform, download=True)
-    elif dataset == 'gtsrb':
-        in_dataset = GTSRB(train=use_train,transform=transform, download=True)
-    elif dataset == 'mnist':
-        in_dataset = torchvision.datasets.MNIST(root=ROOT, train=use_train, download=True, transform=transform)
-    elif dataset == 'fmnist':
-        in_dataset = torchvision.datasets.FashionMNIST(root=ROOT, train=use_train, download=True, transform=transform)
-    else:
-        raise NotImplementedError("In Distribution Dataset not implemented")
-    return in_dataset
 
 def get_ood_loader(in_dataset='cifar10', out_dataset='cifar100', sample=True, sample_num=2000, in_label=1,
                    out_label=0, batch_size=256, in_source='train', out_filter_labels=[],
@@ -99,12 +81,16 @@ def get_ood_loader(in_dataset='cifar10', out_dataset='cifar100', sample=True, sa
         in_dataset = custom_in_dataset
     else:
         if in_source is not None:
-            in_dataset = get_id_dataset(in_dataset, in_source == 'train', in_transform)
+            in_dataset = get_dataset(in_dataset, in_transform, in_source == 'train', custom_label=in_label)
     
     # Out-Distribution Dataset
     if custom_ood_dataset is None:
-        out_dataset = get_ood_dataset(out_dataset, out_transform,
-         in_dataset, out_label=out_label, sample_num=sample_num)
+        if out_dataset == 'rot':
+            out_dataset = deepcopy(in_dataset)
+            out_dataset.transform = transforms.Compose([lambda x: rotate(x, 90), out_dataset.transform])    
+        else:
+            out_dataset = get_ood_dataset(out_dataset, out_transform,
+            in_dataset, out_label=out_label, sample_num=sample_num)
     else:
         out_dataset = custom_ood_dataset
 
@@ -143,7 +129,7 @@ def get_cls_loader(dataset, train=False, sample_portion=0.2, batch_size=256, tra
     if transforms_list:
         transform = transforms.Compose(transforms_list)
     if type(dataset) == str:
-        test_dataset = get_id_dataset(dataset, train, transform)
+        test_dataset = get_dataset(dataset, transform, train)
     else:
         test_dataset = dataset
     if sample_portion < 1:
