a364b303718d3d4c1114d0b51694c6125be8fb7e
Bahar Dibaei Nia <49945055+bhrdbn@users.noreply.github.com>
2024-05-01 20:35:50 +0330

    Add load vit

--- models/loaders.py
+++ models/loaders.py
@@ -6,6 +6,8 @@ from BAD.models.base_model import BaseModel as Model
 from BAD.models.preact import PreActResNet18
 from BAD.models.vgg_custom import CNNClassifier
 from torchvision.models import resnet18
+from torchvision.models import vit_b_16
+
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -44,6 +46,33 @@ def load_resnet(record_path, num_classes=10, **model_kwargs):
     model.eval()
     
     return model
+
+
+def load_vit(record_path, num_classes=10, **model_kwargs):
+    state_dict = torch.load(record_path)
+    new_dict = copy.deepcopy(state_dict['model'])
+    for k, v in state_dict['model'].items():
+        if k.startswith('module.'):
+            del new_dict[k]
+            new_dict[k[7:]] = v
+    state_dict['model'] = new_dict
+    good_dict = {}
+    for x, y in new_dict.items():
+        if x.startswith("1."):
+            good_dict[x[2:]] = y
+    vit_model = vit_b_16(num_classes=num_classes)
+    vit_model.load_state_dict(good_dict)
+    
+    feature_extractor = torch.nn.Sequential(*list(vit_model.children())[:-1])
+
+    model = Model(vit_model, feature_extractor=feature_extractor, **model_kwargs)
+    model.to(device)
+    model.eval()
+    return model
+
+
+
+
 
 def load_vgg(record_path, num_classes=10, **model_kwargs):
     init_num_filters = 64
