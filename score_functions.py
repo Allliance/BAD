14f427f815b7e02b7930761ed9819221a758278b
Allliance <alians310322@gmail.com>
2024-05-07 18:57:01 +0330

    Move get_aucs to score_functions

--- score_functions.py
+++ score_functions.py
@@ -14,6 +14,34 @@ def get_auc(model, dataloader, attack=None, progress=False):
     
     return evaluate(model=model, loader=dataloader, device=device, attack=attack, metric='auc', progress=progress)
 
+
+def get_aucs(model_dataset, eps, progress=False, verbose=False, score_progress=False):
+    attack_eps = eps
+    attack_steps = 10
+    attack_alpha = 2.5 * attack_eps / attack_steps
+    
+    def score_function(model, progress=score_progress):
+        if verbose:
+            print(f"Current model: {model.meta_data['name']}")
+        dataloader = get_dataloader(model)
+        if score != 'final_auc':
+            init_perf = get_auc(model, dataloader, attack=None, progress=progress)
+        
+        attack = Attack(model, eps=attack_eps, steps=attack_steps, alpha=attack_alpha, attack_in=attack_in)
+        if eps == 0:
+            attack = None
+        adv_perf = get_auc(model, dataloader, attack, progress=progress)
+        
+        if score == 'final_auc':
+            return adv_perf
+        elif score == 'prop_auc':
+            return adv_perf / init_perf
+        elif score == 'delta_auc':
+            return init_perf - adv_perf
+
+    return get_models_scores(model_dataset, score_function, progress=progress)
+
+
 def get_l2(model, dataloader, attack=None, use_in=True, progress=False, normalize_features=False):
     
     mean_initial_features = get_features_mean_dict(dataloader,
