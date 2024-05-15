79991073c8c7a6a7b3afd4636a6e074f5994e435
Allliance <alians310322@gmail.com>
2024-05-16 00:50:24 +0330

    Fix bug

--- data/loaders.py
+++ data/loaders.py
@@ -57,7 +57,8 @@ def get_dataset(name, transform=None, train=False,
             
             if channels == 1:
                 new_transforms.append(transforms.Grayscale())
-                
+            elif channels == 3 and name in ['mnist', 'fmnist']:
+                new_transforms.append(transforms.Grayscale())
             new_transforms.append(transforms.Resize((size, size)))
             new_transforms.append(transforms.ToTensor())
             
